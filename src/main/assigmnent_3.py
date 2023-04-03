86f072f5fa766defb9dcba44d3e3b092004f1aef
DiannaMcNair <122403183+DiannaMcNair@users.noreply.github.com>
2023-04-03 06:11:26 +0000

    .

--- src/main/assigmnent_3.py
+++ src/main/assigmnent_3.py
@@ -1,4 +1,4 @@
-import numpy as py
+import numpy as np
 
 
 def euler_method(function, x_0, xfinal, y_0, iterations):
@@ -44,6 +44,36 @@ def runge_kutta(function, xinitial, xfinal, yinitial, iterations):
 
     return y_i
 
+def gaussian_elimination(matrix):
+    size = len(matrix)
+
+    #make all elements below pivot points 0
+    for j in range(size):
+        if matrix[j][j] == 0:
+            break
+
+        for i in range(j, size-1):
+            factor = matrix[i+1][j]/matrix[j][j]
+            matrix[i+1] = matrix[i+1] - (factor*matrix[j])
+
+
+    #back substitution
+    i = size
+    for j in range(i-1, -1, -1):
+
+        matrix[j][i] -= sum(matrix[j][j+1:i])
+
+        solution = matrix[j][i]/matrix[j][j]
+
+        matrix[j][i] = solution
+
+        matrix[:j, j] *= solution
+
+    result = matrix[:,size]
+
+    return result
+
+
 
 #Euler's Method
     #function: t - y^2, range: 0<t<2, iterations: 10, point: f(0)=1
@@ -62,3 +92,11 @@ print(answer)
 
 answer = runge_kutta(function, x_i, x_f, y_i, n)
 print(answer)
+
+
+#Use Gaussian elimination and backward substitution solve the following
+#linear system of equations written in augmented matrix format
+
+matrix = np.array([[2, -1, 1, 6], [1, 3, 1, 0], [-1, 5, 4, -3]], dtype=np.double)
+answer3 = gaussian_elimination(matrix)
+print(answer3)
